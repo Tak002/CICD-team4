3ca98004d488b2d2645e2f5a211c56f53db174b5
jonnykim1o28 <jonnykim1o28@gmail.com>
2025-05-15 21:39:10 +0900

    Stock.hpp 선언

--- src/Stock.hpp
+++ src/Stock.hpp
@@ -1,5 +1,24 @@
 #pragma once
+#include <list>
+#include "Beverage.hpp"
+
+using namespace std;
 
 class Stock {
     
+    private:
+        Beverage beverage;
+        void updateStock(int, int);
+        void DVMMessageOutofStock(int item_id, int item_num);
+        
+
+    public:
+        void requestBeverage(int item_id, int item_num);
+        list<Beverage> getCurrentStock(int beverageId);
+        bool isPrepayment(int item_code, int item_num);
+        bool isBuyable(int item_code, int item_num);
+        void blockSale();
+        void editStock(int item_id, int item_num);
+
+
 };
