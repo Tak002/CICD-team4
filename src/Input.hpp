495d3024bb118e90e3a180dc947277a9f5b1dd30
jonnykim1o28 <jonnykim1o28@gmail.com>
2025-05-15 21:30:26 +0900

    input.hpp 구현

--- src/Input.hpp
+++ src/Input.hpp
@@ -1,5 +1,17 @@
 #pragma once
+#include "CurrentState_enum.hpp"
+#include <string>
+
+using std::string;
 
 class Input {
-    
+    private:
+    string command;
+    CurrentState currentState;
+
+    public: 
+    void sendOperation(CurrentState, string);
+    void printStock();
+    void selectBeverage(int, int);
+    void enterCardNum(int);
 };
