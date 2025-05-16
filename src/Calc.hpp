926244dd9f2270dcb07e84c60f2400773dc8a2aa
jonnykim1o28 <jonnykim1o28@gmail.com>
2025-05-16 13:41:45 +0900

    Calc hpp 구현(미완성)

--- src/Calc.hpp
+++ src/Calc.hpp
@@ -1,5 +1,11 @@
 #pragma once
+#include <string>
+
 
 class Calc {
-    
+    private:
+        std::string nearestID;
+    public:
+        int nearestPosition(int coor_x, int coor_y);
+        
 };
