678a993acc8e5077369ddb16dd987eab31e1d221
jonnykim1o28 <jonnykim1o28@gmail.com>
2025-05-16 11:57:16 +0900

    CertCode 구현

--- src/CertCode.cpp
+++ src/CertCode.cpp
@@ -1,2 +1,30 @@
 // CertCode.cpp
 #include "CertCode.hpp"
+#include <iostream>
+#include <string>
+#include <random>
+#include <ctime>
+
+std::string CertCode::generateRandomString(size_t length) {
+    const std::string charset =
+        "0123456789"
+        "ABCDEFGHIJKLMNOPQRSTUVWXYZ"
+        "abcdefghijklmnopqrstuvwxyz";
+    
+    std::string result;
+    result.reserve(length);
+
+    std::mt19937 engine(static_cast<unsigned long>(std::time(nullptr)));
+    std::uniform_int_distribution<> dist(0, charset.size() - 1);
+
+    for (size_t i = 0; i < length; ++i) {
+        result += charset[dist(engine)];
+    }
+
+    return result;    
+}
+
+void CertCode::createCertCode(){
+    string code = generateRandomString(5);
+    Controller:: getInstance.sendPrePaymentMSG(code);
+}
--- src/CertCode.hpp
+++ src/CertCode.hpp
@@ -1,5 +1,11 @@
 #pragma once
+#include <string>
+
+using namespace std;
 
 class CertCode {
-    
+    private:
+        string generateRandomString(size_t length);
+    public:
+        void createCertCode();
 };
