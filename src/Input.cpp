b5a8cda7c5df7ef98ad120830b42332576cf7a29
Tak002 <andycho1120@naver.com>
2025-05-17 16:07:40 +0900

    Feat: input have contorller

--- src/Input.cpp
+++ src/Input.cpp
@@ -1,2 +1,10 @@
 // Input.cpp
 #include "Input.hpp"
+
+Input::Input(Controller* ctrl){
+    controller = ctrl;
+}
+
+void Input::run(){
+    
+}
--- src/Input.hpp
+++ src/Input.hpp
@@ -1,15 +1,18 @@
 #pragma once
 #include "CurrentState_enum.hpp"
 #include <string>
-
+#include <Controller.hpp>
 using std::string;
 
 class Input {
     private:
+    Controller* controller;
     string command;
     CurrentState currentState;
 
     public: 
+    Input(Controller *ctrl);
+    void run();
     void sendOperation(CurrentState, string);
     void printStock();
     void selectBeverage(int, int);
