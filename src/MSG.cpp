2750a5f2ecdf41ba5af8d9ce7ad9b118c31a564f
정석현 <codjeongsh02@gmail.com>
2025-05-28 20:02:16 +0900

    Catch a more specific exception instead of a generic one.
    이전에 남긴 catch문을 안 지워 생긴 오류

--- src/MSG.cpp
+++ src/MSG.cpp
@@ -183,11 +183,6 @@ void clientMessage(const std::string &dst_id, const json &msg)
         std::cerr << "[ERROR] JSON 타입 오류 (예상과 다른 타입): " << e.what() << std::endl;
         return;
     }
-    catch (const std::exception &e)
-    {
-        std::cerr << "[ERROR] 알 수 없는 JSON 처리 오류: " << e.what() << std::endl;
-        return;
-    }
 
     if (ip_data.contains(dst_id))
     {
