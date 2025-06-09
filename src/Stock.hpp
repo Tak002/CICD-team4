4f71c200617f5e6d2d002945a241c5c4006d687f
jonnykim1o28 <jonnykim1o28@gmail.com>
2025-06-09 11:45:00 +0900

    code smell

--- src/Stock.hpp
+++ src/Stock.hpp
@@ -9,12 +9,12 @@ class Stock {
     private:
         
         Beverage bev_to_buy; // 현재 구매 시도중인 음료
-        void updateStock(int, int);
+        void updateStock(const int, const int);
         
 
     public:
         void requestBeverage(int item_id, int item_num);
-        std::list<Beverage> getCurrentStock();
+        std::list<Beverage> getCurrentStock() const;
         bool isPrepayment(int item_code, int item_num);
         bool isBuyable(std::string certCode,int item_code, int item_num);
         void blockSale();
